7159fc1dab0c86530d05449c0c12735a8fda9501
Jaime Sendra <jsendra@autis.es>
2020-04-16 11:26:46 +0200

    setup

--- setup.py
+++ setup.py
@@ -8,12 +8,15 @@ setup(
   author = 'Jaime Sendra',                   # Type in your name
   author_email = 'jaisenberafel@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/jaisenbe58r/Ejemplo_pypi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/jaisenbe58r/Ejemplo_pypi/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/jaisenbe58r/Ejemplo_pypi/archive/v0.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'matplotlib',
+          'pandas',
+          'scikit-learn',
+          'pytest'
       ],
+
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
