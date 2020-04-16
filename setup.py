b2c932162304f924dfd33ee9490248c1cebac69e
Jaime Sendra <jsendra@autis.es>
2020-04-16 11:34:14 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'MyLib',         # How you named your package folder (MyLib)
-  packages = ['MyLib'],   # Chose the same as "name"
+  name = 'MyLib_JS',         # How you named your package folder (MyLib)
+  packages = ['MyLib_JS'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'How to upload your python package to PyPi',   # Give a short description about your library
